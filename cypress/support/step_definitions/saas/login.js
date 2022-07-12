926a76d001d3f5a623c97f94353a10df0b5279ca
Albert <albert.wirawan001@gmail.com>
2022-07-12 14:19:49 +0700

    update login and registration flow - add SAAS prefix

--- cypress/support/step_definitions/saas/login.js
+++ cypress/support/step_definitions/saas/login.js
@@ -21,62 +21,61 @@ function generateRandomNumber(num) {
     return "8"+num+"";
 }
 
-Given('a user is on log in page', () => {
+Given('SAAS - a user is on log in page', () => {
     loginPage.visitLogin();
 });
 
-When('user clicks on start registration button', () => {
+When('SAAS - user clicks on start registration button', () => {
     loginPage.clickStartRegisterButton();
 });
 
-When('user fills phone number input with {string}', (number) => {
+When('SAAS - user fills phone number input with {string}', (number) => {
     loginPage.typeNumber(number);
 });
 
-When('user selects whatsapp otp type', () => {
+When('SAAS - user selects whatsapp otp type', () => {
     loginPage.selectWhatsappOtpType();
 });
 
-When('user selects sms otp type', () => {
+When('SAAS - user selects sms otp type', () => {
     loginPage.selectSmsOtpType();
 });
 
-When('user clicks on send otp button', () => {
+When('SAAS - user clicks on send otp button', () => {
     loginPage.clickSendOtp();
 });
 
-When('user clicks on submit otp button', () => {
+When('SAAS - user clicks on submit otp button', () => {
     loginPage.clickSubmitOtp();
     cy.wait(1500);
 });
 
 
-When('user fills the otp input with correct otp', () => {
+When('SAAS - user fills the otp input with correct otp', () => {
     loginPage.inputOtp1(otpInput1);
     loginPage.inputOtp2(otpInput2);
     loginPage.inputOtp3(otpInput3);
     loginPage.inputOtp4(otpInput4);
 });
 
-When('user fills the otp input with wrong otp', () => {
+When('SAAS - user fills the otp input with wrong otp', () => {
     loginPage.inputOtp1("9");
     loginPage.inputOtp2("9");
     loginPage.inputOtp3("9");
     loginPage.inputOtp4("9");
 });
 
-When('user clicks on masuk button', () => {
+When('SAAS - user clicks on masuk button', () => {
     loginPage.clickLogin();
 });
 
 // assertions
 
-Then('{string} displayed after successfully logged in', (expected) => {
-    cy.wait(1500);
+Then('SAAS - {string} displayed after successfully logged in', (expected) => {
     expect(cy.get("h2").should("contain.text",expected));
 });
 
-Then('{string} displayed after submitting otp code', (expected) => {
+Then('SAAS - {string} displayed after submitting otp code', (expected) => {
     expect(cy.get(basePage.snackbar_error).should("contain.text",expected));
 });
 
--- cypress/support/step_definitions/saas/registration.js
+++ cypress/support/step_definitions/saas/registration.js
@@ -2,6 +2,7 @@ import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 import RegistrationPage from "../../../integration/saas/page_objects/RegistrationPage";
 import LoginPage from "../../../integration/saas/page_objects/LoginPage";
 
+
 const registrationPage = new RegistrationPage();
 
 let random = Math.floor(100000000 + Math.random() * 900000000);
@@ -9,64 +10,66 @@ function generateRandomNumber(num) {
     return "8"+num+"";
 }
 
-Given('a user is on registration page', () => {
+Given('SAAS - a user is on registration page', () => {
+    cy.clearCookies();
     registrationPage.visitRegistration();
 });
 
-When('user fills phone number with random phone number',() => {
+When('SAAS - user fills phone number with random phone number',() => {
     registrationPage.typeNumber(generateRandomNumber(random));
 });
 
-When('user clicks on lanjutkan button #no. handphone', () => {
+When('SAAS - user clicks on lanjutkan button #no. handphone', () => {
     registrationPage.clickRegisterLanjutkan1();
 });
 
-When('user clicks on lanjutkan button #data diri', () => {
+When('SAAS - user clicks on lanjutkan button #data diri', () => {
     registrationPage.clickRegisterLanjutkan2();
 });
 
-When('user fills name field with {string}', (name) => {
+When('SAAS - user fills name field with {string}', (name) => {
     registrationPage.typeName(name);
 });
 
-When('user fills email field with {string}', (email) => {
+When('SAAS - user fills email field with {string}', (email) => {
     registrationPage.typeEmail(email);
 });
 
-When('user clicks on daftar button', () => {
+When('SAAS - user clicks on daftar button', () => {
     registrationPage.clickSubmitRegistrationButton();
 });
 
-When('user fills store name field with {string}', (storeName) => {
+When('SAAS - user fills store name field with {string}', (storeName) => {
     registrationPage.typeStoreName(storeName);
 });
 
-When('user clicks on input address button', () => {
+When('SAAS - user clicks on input address button', () => {
     registrationPage.clickStoreAddressInputButton();
 });
 
-When('user fills store address field with {string}', (address) => {
+When('SAAS - user fills store address field with {string}', (address) => {
     registrationPage.typeStoreAddressLocationInput(address);
 });
 
-When('user clicks on current address button', () => {
+When('SAAS - user clicks on current address button', () => {
     registrationPage.clickStoreCurrentLocationButton();
 });
 
-When('user clicks on manual address button', () => {
+When('SAAS - user clicks on manual address button', () => {
     registrationPage.clickStoreManualAddressButton();
 });
 
-When('user clicks on first store address search result', () => {
+When('SAAS - user clicks on first store address search result', () => {
     registrationPage.clickFirstAddressResult();
 });
 
-When('user clicks on choose this location', () => {
+When('SAAS - user clicks on choose this location', () => {
     registrationPage.clickChooseStoreLocation();
 });
 
-When('user clicks on submit registration button', () => {
+When('SAAS - user clicks on daftar button', () => {
     registrationPage.clickSubmitRegistrationButton();
+    cy.wait(2000);
 });
 
 // assertions
