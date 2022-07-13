8931da98ee23c8191dedc526098f1b3b46990a17
fiqah-majidah <rafiqah.majidah@gudangada.com>
2022-07-13 14:23:58 +0700

    set up environment configuration

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,20 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
+const fs = require("fs-extra");
+const path = require("path");
+
+const fetchConfigurationByFile = file => {
+    const pathOfConfigurationFile = `config/cypress.${file}.json`;
+
+    return (
+        file && fs.readJson(path.join(__dirname, "../", pathOfConfigurationFile))
+    );
+};
 
 module.exports = (on, config) => {
     on('file:preprocessor', cucumber())
-}
+    const environment = config.env.configFile || "development";
+    const configurationForEnvironment = fetchConfigurationByFile(environment);
+
+    return configurationForEnvironment || config;
+};
+
