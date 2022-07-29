466fe8a9f3db72cc393f24eb540d81f69469be4b
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2022-07-29 09:47:26 +0700

    add prettier settings

--- cypress/integration/saas/page_objects/InventoryListPage.js
+++ cypress/integration/saas/page_objects/InventoryListPage.js
@@ -1,33 +1,30 @@
-import BasePage from './BasePage.js'
+import BasePage from "./BasePage.js";
 
 export default class InventoryListPage extends BasePage {
+  path = "inventory/list";
+  addInventoryButton = "button[id='button_add_inventory']";
+  firstTimeAddInventoryButton = "button[id='button_first_time_add_inventory']";
+  searchByNameInput = "input[id='input_search_inventory_by_name']";
+  addCustomInventoryButton = "span[id='button_add_custom_product']";
 
-    path = "inventory/list";
-    addInventoryButton = "button[id='button_add_inventory']";
-    firstTimeAddInventoryButton = "button[id='button_first_time_add_inventory']";
-    searchByNameInput = "input[id='input_search_inventory_by_name']";
-    addCustomInventoryButton = "span[id='button_add_custom_product']";
+  // common
+  visitInventoryList() {
+    cy.visit(this.baseUrl + this.path);
+  }
 
-    // common
-    visitInventoryList(){
-        cy.visit(this.baseUrl+this.path);
-    }
+  clickAddInventory() {
+    cy.get(this.addInventoryButton).click();
+  }
 
-    clickAddInventory() {
-        cy.get(this.addInventoryButton).click();
-    }
+  clickFirstTimeAddInventoryButton() {
+    cy.get(this.firstTimeAddInventoryButton).click();
+  }
 
-    clickFirstTimeAddInventoryButton() {
-        cy.get(this.firstTimeAddInventoryButton).click();
-    }
+  typeInventoryName(inventoryName) {
+    cy.get(this.searchByNameInput).type(inventoryName);
+  }
 
-    typeInventoryName(inventoryName) {
-        cy.get(this.searchByNameInput).type(inventoryName);
-    }
-
-    clickAddCustomInventoryButton() {
-        cy.get(this.addCustomInventoryButton).click();
-    }
-
-
+  clickAddCustomInventoryButton() {
+    cy.get(this.addCustomInventoryButton).click();
+  }
 }
--- cypress/support/step_definitions/saas/homePage.js
+++ cypress/support/step_definitions/saas/homePage.js
@@ -3,21 +3,21 @@ import HomePage from "../../../integration/saas/page_objects/HomePage";
 
 const homePage = new HomePage();
 
-Given('SAAS - a user is on home page', () => {
-    cy.clearCookies();
-    homePage.visitHomePage();
+Given("SAAS - a user is on home page", () => {
+  cy.clearCookies();
+  homePage.visitHomePage();
 });
 
-When('SAAS - user clicks close tutorial button',() => {
-    cy.wait(5000);
-    homePage.clickCloseTutorialButton();
+When("SAAS - user clicks close tutorial button", () => {
+  cy.wait(5000);
+  homePage.clickCloseTutorialButton();
 });
 
-When('SAAS - user clicks confirm close tutorial button', () => {
-    homePage.clickConfirmCloseTutorialButton();
+When("SAAS - user clicks confirm close tutorial button", () => {
+  homePage.clickConfirmCloseTutorialButton();
 });
 
-When('SAAS - user clicks cancel close tutorial button', () => {
-    homePage.clickCancelCloseTutorialButton();
+When("SAAS - user clicks cancel close tutorial button", () => {
+  homePage.clickCancelCloseTutorialButton();
 });
 // assertions
