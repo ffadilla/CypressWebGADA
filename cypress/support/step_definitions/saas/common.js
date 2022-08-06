a5575e0d28554e2cb8d726867c0ef8dd6f0dfcfe
Albert <albert.wirawan001@gmail.com>
2022-08-06 19:46:13 +0700

    add dependencies, replace methods with utils method

--- cypress/support/step_definitions/saas/common.js
+++ cypress/support/step_definitions/saas/common.js
@@ -2,54 +2,32 @@ import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 import LoginPage from '../../../integration/saas/page_objects/LoginPage';
 import RegistrationPage from '../../../integration/saas/page_objects/RegistrationPage';
 import HomePage from "../../../integration/saas/page_objects/HomePage";
+import * as utils from "../saas/utils";
 
 const loginPage = new LoginPage();
 const registrationPage = new RegistrationPage();
 const homePage = new HomePage();
 
-let today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0');
-let otpInput1 = mm.charAt(0);
-let otpInput2 = mm.charAt(1);
-let otpInput3 = dd.charAt(0);
-let otpInput4 = dd.charAt(1);
-let randomNumber = Math.floor(100000000 + Math.random() * 900000000);
-let randomString = generateRandomString();
-
-function generateRandomNumber(num) {
-    return "8"+num+"";
-}
-
-function generateRandomString() {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < 5; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() *
-            charactersLength));
-    }
-    return result;
-}
+let otp = utils.generateCurrentDateOTP();
 
 Given('SAAS - a new user is registered', () => {
     cy.clearLocalStorage();
     cy.clearCookies();
     cy.reload(true);
     registrationPage.visitRegistration();
-    registrationPage.typeNumber(generateRandomNumber(registrationPage));
+    registrationPage.typeNumber(utils.generateRandomNumber());
     registrationPage.clickRegisterLanjutkan1();
     loginPage.selectWhatsappOtpType();
     loginPage.clickSendOtp();
-    loginPage.inputOtp1(otpInput1);
-    loginPage.inputOtp2(otpInput2);
-    loginPage.inputOtp3(otpInput3);
-    loginPage.inputOtp4(otpInput4);
+    loginPage.inputOtp1(otp.charAt(0));
+    loginPage.inputOtp2(otp.charAt(1));
+    loginPage.inputOtp3(otp.charAt(2));
+    loginPage.inputOtp4(otp.charAt(3));
     loginPage.clickSubmitOtp();
-    registrationPage.typeName(randomString);
-    registrationPage.typeEmail(randomString+"@gudangada.com");
+    registrationPage.typeName(utils.generateRandomString(5));
+    registrationPage.typeEmail(utils.generateRandomString(5)+"@gudangada.com");
     registrationPage.clickRegisterLanjutkan2();
-    registrationPage.typeStoreName(randomString);
+    registrationPage.typeStoreName(utils.generateRandomString(5));
     registrationPage.clickStoreAddressInputButton();
     registrationPage.typeStoreAddressLocationInput("jakarta");
     registrationPage.clickFirstAddressResult();
@@ -68,10 +46,10 @@ Given('SAAS - user {string} is logged in', (number) => {
     loginPage.clickLogin();
     loginPage.selectWhatsappOtpType();
     loginPage.clickSendOtp();
-    loginPage.inputOtp1(otpInput1);
-    loginPage.inputOtp2(otpInput2);
-    loginPage.inputOtp3(otpInput3);
-    loginPage.inputOtp4(otpInput4);
+    loginPage.inputOtp1(otp.charAt(0));
+    loginPage.inputOtp2(otp.charAt(1));
+    loginPage.inputOtp3(otp.charAt(2));
+    loginPage.inputOtp4(otp.charAt(3));
     loginPage.clickSubmitOtp();
 });
 
