fc48d1bbccacb7c84039dc27ee69b24c47a37252
Albert <albert.wirawan001@gmail.com>
2022-08-12 10:00:37 +0700

    - remove logout custom command on global.d.ts and utils.ts - move login custom command to index.ts

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -25,6 +25,9 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 // TODO: change any
+import gadaConfig from "../e2e/utils/gadaConfig";
+import { generateCurrentDateOTP } from "./step_definitions/saas/utils";
+
 Cypress.Commands.add("randomNumber", (length: number): any => {
   return parseInt(
     ("" + Math.random()).substring(2, 2 + length).replace(/0/g, "1")
@@ -36,4 +39,34 @@ Cypress.Commands.add("numberFormat", (number: number): any => {
   return new Intl.NumberFormat("id-ID", { minimumFractionDigits: 0 }).format(
     number
   );
+});
+
+// SAAS Custom Commands
+
+Cypress.Commands.add("saasLogin", (phoneNumber: string): any => {
+  cy.request({
+    method: "POST",
+    url: gadaConfig.saas.baseApiUrl + "authentication/otp/start",
+    body: {
+      client_id: "zfS2K2_mgXnevZt0rO1No1OQm-8zZWbwwIuO",
+      otp_type: "WHATSAPP",
+      verification_type: "LOGIN",
+      phone_number: phoneNumber,
+    },
+  });
+  cy.request({
+    method: "POST",
+    url: gadaConfig.saas.baseApiUrl + "authentication/oauth/token",
+    body: {
+      audience: "https://gudangada.com/saas-api",
+      client_id: "zfS2K2_mgXnevZt0rO1No1OQm-8zZWbwwIuO",
+      otp_type: "WHATSAPP",
+      otp_code: generateCurrentDateOTP(),
+      verification_type: "LOGIN",
+      phone_number: phoneNumber,
+    },
+  }).then((resp) => {
+    window.localStorage.setItem("AUTH_TOKEN", resp.body.data.access_token);
+    window.localStorage.setItem("IS_LOGGED_IN", "true");
+  });
 });
