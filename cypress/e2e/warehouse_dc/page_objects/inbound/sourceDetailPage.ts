362e9c7be287a558f18c6ee23819765154305110
GADA-399\GADA <faris.ihsaan.fauzi@gmail.com>
2022-11-24 10:32:10 +0700

    feat: implement get source detail flow at WMS DC

---
+++ cypress/e2e/warehouse_dc/page_objects/inbound/sourceDetailPage.ts
@@ -0,0 +1,3 @@
+import CoreSourceDetailPage from "../../../warehouse_core/page_objects/inbound/sourceDetailPage";
+
+export default class SourceDetailPage extends CoreSourceDetailPage {}
---
+++ cypress/e2e/warehouse_dc/step_definitions/inbound/sourceDetail.ts
@@ -0,0 +1,59 @@
+import { Then, When } from "@badeball/cypress-cucumber-preprocessor";
+import SourceDetailPage from "../../page_objects/inbound/sourceDetailPage";
+
+const sourceDetailPage = new SourceDetailPage("mitra");
+
+When("user cancels Source at inbound Source detail", () => {
+  sourceDetailPage.cancelSource();
+});
+
+When("user clicks historical reception button at inbound Source detail", () => {
+  sourceDetailPage.clickHistoryCTA();
+});
+
+When(
+  "user should be able to download {string} document on inbound Source detail's history popup",
+  (value: string) => {
+    sourceDetailPage.downloadSourceAttachment(value);
+  }
+);
+
+When("user closes historical reception popup at inbound Source detail", () => {
+  sourceDetailPage.closeHistoryPopup();
+});
+
+Then(
+  "user should be at inbound Source detail page with {string} Request",
+  (status: string) => {
+    expect(cy.url().should("include", sourceDetailPage.path));
+    sourceDetailPage.assertSourceUI(status);
+  }
+);
+
+Then(
+  "user should see similar inbound Source data between detail page and {string} Request",
+  (status: string) => {
+    sourceDetailPage.assertSourceDataByRequestDetail(status);
+  }
+);
+
+Then(
+  "user should see similar inbound Source data between detail page and inbound form",
+  () => {
+    sourceDetailPage.assertSourceDataByInboundForm();
+  }
+);
+
+Then(
+  "user should see similar inbound Source data between historical reception and request data",
+  () => {
+    sourceDetailPage.assertHistoryDataByRequestDetail();
+  }
+);
+
+Then(
+  "user should see similar inbound Source data between historical reception and submitted Receipt data",
+  () => {
+    sourceDetailPage.assertHistoryDataByReceiptSubmission();
+  }
+);
